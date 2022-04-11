ba7c9275c8b831a1f3f0da97e43bbe75c34bdf8c
Michael Vork <mwvork@gmail.com>
2022-04-11 15:28:19 -0400

    Fixed Package.swift.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CallyoContacts",
-            path: "CallyoContacts/Sources/CallyoContacts"
+            path: "CallyoContacts/Sources/CallyoContacts",
             dependencies: [],
             resources: [
                 .process("Resources")
