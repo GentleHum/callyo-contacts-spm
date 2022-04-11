53ace9abcbcc021227927b8f3a01ec16f3449b9e
Michael Vork <mwvork@gmail.com>
2022-04-11 13:53:03 -0400

    Added basic package files and folders.

--- CallyoContacts/Sources/CallyoContacts/API/CallyoContacts.swift
+++ CallyoContacts/Sources/CallyoContacts/API/CallyoContacts.swift
@@ -1,6 +1,44 @@
-public struct CallyoContacts {
-    public private(set) var text = "Hello, World!"
+//import UIKit
+//
+//public struct CallyoContacts {
+//    public private(set) var text = "Hello, World!"
+//    public private(set) var storyboard: UIStoryboard?
+//
+//    public init() {
+//        storyboard = UIStoryboard(name: "CallyoContacts", bundle: Bundle.module)
+//    }
+//}
 
-    public init() {
+import UIKit
+
+@objc public protocol CallyoContactsActionDelegate {
+    @objc optional func getSubscriptionWasChosen()
+    @objc optional func makeCallWasChosen(for phoneNumber: String)
+    @objc optional func makeVideoCallWasChosen(for phoneNumber: String)
+    @objc optional func sendTextWasChosen(for phoneNumber: String)
+}
+
+public class CallyoContacts {
+    
+    public static var delegate: CallyoContactsActionDelegate? = nil
+    
+    public static func embedController(
+        in parentController: UIViewController,
+        frame: CGRect,
+        delegate: CallyoContactsActionDelegate?
+    ) -> CallyoContactsViewController? {
+        self.delegate = delegate
+        let storyboard = UIStoryboard(name: "CallyoContacts", bundle: Bundle.module)
+        if let contactsController = storyboard.instantiateInitialViewController() as? CallyoContactsViewController {
+            parentController.addChild(contactsController)
+            parentController.view.addSubview(contactsController.view)
+            contactsController.view.frame = frame
+            contactsController.view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
+            contactsController.didMove(toParent: parentController)
+            contactsController.delegate = delegate
+            return contactsController
+        }
+        
+        return nil
     }
 }
---
+++ CallyoContacts/Sources/CallyoContacts/Controllers/CallyoContactsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CallyoContactsViewController.swift
+//  
+//
+//  Created by Michael Vork on 4/7/22.
+//
+
+import UIKit
+
+public class CallyoContactsViewController: UIViewController {
+    public var delegate: CallyoContactsActionDelegate? = nil
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
